6aa4544a6782d1322edb5f9c8c5b09b46798d997
julianamacedo1 <julianamacedo@knights.ucf.edu>
2021-03-07 20:27:53 -0500

    March 7, 2021 8:27 PM

--- my-names-chug/src/App.js
+++ my-names-chug/src/App.js
@@ -1,3 +1,12 @@
+import React from "react"
 import './App.css';
 
+class HomePage extends React.Component {
 
+}
+
+export default class App extends React.Component {
+    render() {
+        return <HomePage></HomePage>
+    }
+} 
