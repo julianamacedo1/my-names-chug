98a193777692f55584a130149532e51096971f72
julianamacedo1 <julianamacedo@knights.ucf.edu>
2021-03-07 22:04:07 -0500

    March 7, 2021 10:04 PM

--- my-names-chug/src/App.js
+++ my-names-chug/src/App.js
@@ -2,6 +2,8 @@ import React from "react"
 import "./App.css"
 import "./image/rand"
 
+export
+
 export default class App extends React.Component {
     render() {
         return <RandDrink></RandDrink>
