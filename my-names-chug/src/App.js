23193a377ea619c46cc58b6471327d52122b4e13
julianamacedo1 <julianamacedo@knights.ucf.edu>
2021-03-09 21:08:46 -0500

    March 9, 2021 9:08 PM

--- my-names-chug/src/App.js
+++ my-names-chug/src/App.js
@@ -1,14 +1,10 @@
 import React from "react"
 import "./App.css"
-import "./image/rand"
 
 class RandDrink extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            font_size: "22",
-            topText: "",
-            bottomText: "",
             randomImg: "./image/rand",
             allDrinkImgs: []
         };
@@ -21,7 +17,7 @@ class RandDrink extends React.Component {
         let randomNumber = Math.floor(
             Math.random() * this.state.allDrinkImgs.length
         );
-        this.setState({ randomImg: this.state.allDrinkImgs[randomNumber] })
+        this.setState({ randomImg: this.state.allDrinkImgs[randomNumber].url })
     };
     increaseFont = () => {};
     componentDidMount() {
@@ -34,49 +30,7 @@ class RandDrink extends React.Component {
     }
     render() {
         return (
-            <div>
-        <div className="meme-form">
-          <input
-            type="text"
-            name="topText"
-            placeholder="top text"
-            onChange={this.handleChange}
-            value={this.state.topText}
-          />
-          <br />
-          <input
-            type="text"
-            name="bottomText"
-            placeholder="bottom text"
-            onChange={this.handleChange}
-            value={this.state.bottomText}
-          />
-          <br />
-          <input
-            type="number"
-            name="font_size"
-            placeholder="font size"
-            onChange={this.handleChange}
-            value={this.state.font_size}
-          />
-          <button onClick={this.handleClick}>Generate!</button>
-        </div>
-        <div className="meme">
-          <h2
-            style={{ fontSize: Number(this.state.font_size) }}
-            className="top"
-          >
-            {this.state.topText}
-          </h2>
-          <img src={this.state.randomImg} alt="" />
-          <h2
-            style={{ fontSize: Number(this.state.font_size) }}
-            className="bottom"
-          >
-            {this.state.bottomText}
-          </h2>
-        </div>
-      </div>
+            
         );
     }
 }
