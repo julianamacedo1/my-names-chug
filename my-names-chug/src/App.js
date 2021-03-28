3598acc430d79864bbf24ddac6c90e521cad24ef
julianamacedo1 <julianamacedo@knights.ucf.edu>
2021-03-28 14:54:21 -0400

    March 28, 2021 2:54 PM

--- my-names-chug/src/App.js
+++ my-names-chug/src/App.js
@@ -2,13 +2,9 @@ import React from "react"
 import "./App.css"
 
 class RandDrink extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            randomImg: "./components/images",
-            allDrinkImgs: []
-        }
-    }
+    
+    
+
     handleClick = () => {
         this.
     }
