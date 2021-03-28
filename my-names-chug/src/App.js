cd3ffa5e5d99406238d188ef9637f546a35e003c
julianamacedo1 <julianamacedo@knights.ucf.edu>
2021-03-28 18:55:48 -0400

    March 28, 2021 6:55 PM

--- my-names-chug/src/App.js
+++ my-names-chug/src/App.js
@@ -1,23 +1,3 @@
 import React, { useState } from "react"
 import "./App.css"
 
-class RandDrink extends React.Component {
-    
-    handleClick = () => {
-
-    }
-    
-    render() {
-        return (
-            <>
-            <button onClick={() => this.handleClick}>generate!</button>
-            </>
-        );
-    }
-}
-
-export default class App extends React.Component {
-    render() {
-        return <RandDrink></RandDrink>
-    }
-} 
